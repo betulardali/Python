cd3269542ae5ea4a1c4d0e22c3b3e8cd4c9f366d
Betül Ardalı <120562739+betulardali@users.noreply.github.com>
2023-05-08 09:49:43 +0300

    Update standart_deviation.py

--- standart_deviation.py
+++ standart_deviation.py
@@ -15,10 +15,11 @@ num_7=randint(1,100)
 print(num_1 , num_2 , num_3, num_4 , num_5 , num_6 , num_7)
 
 mean=(num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7) / 7
-#if we want to make it integer; ort=int((num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7) / 7)
+#if we want to make it integer; mean=int((num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7) / 7)
 print("arithmetic mean:  ",mean)
 
-#let's say we have an array that has n elements. To find standart deviation, we substract every number from arithmetic mean by one by and take square of them. And then we sum all squares and take divison by n-1. Finally we take square root.
+'''Let's say we have an array that has n elements. To find standart deviation, we substract every number from arithmetic mean by one by and take square of them.
+And then we sum all squares and take divison by n-1. Finally we take square root.'''
 
 s_1=mean-num_1
 s_1*=s_1
@@ -55,7 +56,7 @@ print("Sum of squares: ",sum)
 deviation= sum / 6
 import math
 standart_deviation=math.sqrt(deviation)
-#integer >>  standart_sapma=int(math.sqrt(sapma))
+#integer >>  standart_deviation=int(math.sqrt(sapma))
 print("Standart deviation: ",standart_deviation)
 
 
